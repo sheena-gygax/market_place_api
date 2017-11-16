b6b848d3d712320c46b8dfacfabfb602208f4d2b
Sheena Gygax <sheena.gygax@getg5.com>
2017-11-15 21:24:04 -0800

    Adds destroy session

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -37,4 +37,13 @@ RSpec.describe Api::V1::SessionsController, type: :controller do
       it { should respond_with 422 }
     end
   end
+
+  describe "DELETE #destroy" do
+    before(:each) do 
+      @user = FactoryGirl.create :user
+      sign_in @user
+      delete :destroy, params: { id: @user.auth_token }
+    end
+    it { should respond_with 204 }
+  end
 end
